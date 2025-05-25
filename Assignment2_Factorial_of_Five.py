c3b1a564aa1b311591e61b6f30673bcfd972e47a
David Makwetta <davidmakwetta@gmail.com>
2025-05-26 00:17:30 +0300

    Completed Assignment 2 and 3

---
+++ Assignment2_Factorial_of_Five.py
@@ -0,0 +1,10 @@
+#Find  the factorial of the number 5, 5!
+def find_factorial(num):
+    factorial = 1
+    i = 1
+    while i <= num:
+        factorial *= i
+        i += 1
+    return factorial
+
+print("The factorial of 5 is", find_factorial(5))
